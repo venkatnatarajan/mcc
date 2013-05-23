74c6bf08df587b10fb40d6f0a1280bcddcfa1254
Anthony Felice <tony.felice@timesys.com>
2013-05-23 15:34:38 -0400

    libmcc: Add check to mcc_send() to ensure msg_size does not exceed max size of ATTR_BUFFER.

--- linux/libmcc-1.0/libmcc-1.0/src/libmcc.c
+++ linux/libmcc-1.0/libmcc-1.0/src/libmcc.c
@@ -23,6 +23,7 @@
 
 #include <linux/mcc_common.h>
 #include <linux/mcc_linux.h>
+#include <linux/mcc_config.h>
 
 #include <stdio.h>
 #include <unistd.h>
@@ -226,6 +227,8 @@ int set_io_modes(int set_endpoint_command, MCC_ENDPOINT *current_endpoint, MCC_E
  */
 int mcc_send(MCC_ENDPOINT *endpoint, void *msg, MCC_MEM_SIZE msg_size, unsigned int timeout_us)
 {
+	if(msg_size > MCC_ATTR_BUFFER_SIZE_IN_BYTES)
+		return MCC_ERR_INVAL;
 	int retval = set_io_modes(MCC_SET_SEND_ENDPOINT, &send_endpoint, endpoint, timeout_us);
 	if(retval)
 		return retval;
